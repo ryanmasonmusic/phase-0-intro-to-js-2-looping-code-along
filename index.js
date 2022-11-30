f9541e1a96780fe8cb9054d5156ace05ff95b333
Ryan Mason <ryanmasonmusic@gmail.com>
2022-11-29 22:30:26 -0500

    commit tree a8b3169e084d3c0c41d626788190c9bd6fa2f3aa

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(names, surprise){
+    const card = []
+for (let i = 0; i < names.length; i++){
+    card.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+}
+return card
+}
+function countDown(){
+    let i = 10;
+    while (i >= 0) {
+    console.log(i--)
+    }
+    return i
+}
